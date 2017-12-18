ab33b17beb9562d5ffc025e0c4b8c0e01b3266e3
David Barragán Merino <david.barragan@kaleidos.net>
2017-12-18 12:50:02 +0100

    Initial version :baby:

---
+++ src/components/PmMap/PmMap.vue
@@ -0,0 +1,21 @@
+<template src='./PmMap.html'></template>
+<style src='./PmMap.css' scoped></style>
+
+<script>
+import Vue2Leaflet from 'vue2-leaflet'
+
+export default {
+  name: 'PmMap',
+  components: {
+    'v-map': Vue2Leaflet.Map,
+    'v-tilelayer': Vue2Leaflet.TileLayer,
+    'v-marker': Vue2Leaflet.Marker
+  },
+  data: () => ({
+    zoom: 14,
+    center: [40.4360, -3.6714],
+    url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
+    attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+  })
+}
+</script>
---
+++ src/pages/PmHome/PmHome.vue
@@ -0,0 +1,13 @@
+<template src='./PmHome.html'></template>
+<style src='./PmHome.css'></style>
+
+<script>
+import PmMap from '@/components/PmMap/PmMap'
+
+export default {
+  name: 'PmHome',
+  components: {
+    'pm-map': PmMap
+  }
+}
+</script>
