54ca5c4cd6d0cf81afecbe01aec9c50ad55c0946
Juan de la Cruz <juan.delacruz@kaleidos.net>
2017-12-21 15:53:00 +0100

    add comments form styles

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const baseEnv = require('./base.env')
 module.exports = {
   ...baseEnv,
   NODE_ENV: '"development"',
-  API_SERVER_URL: '"http://localhost:8000/api/"'
+  API_SERVER_URL: '"http://92.243.6.13:8000/api/"'
 }
