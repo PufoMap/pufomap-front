c90243a2eb734a4c5468a63aa58afb4b777b94a7
David Barragán Merino <david.barragan@kaleidos.net>
2017-12-22 00:37:33 +0100

    Change API url in production env

--- config/prod.env.js
+++ config/prod.env.js
@@ -6,5 +6,5 @@ const baseEnv = require('./base.env')
 module.exports = {
   ...baseEnv,
   NODE_ENV: '"production"',
-  API_SERVER_URL: '"http://92.243.6.13:8000/api/"'
+  API_SERVER_URL: '"http://magufomap.com:8000/api/"'
 }
